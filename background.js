8d03714a20b607b60a608e0347bba2e14c6813af
Nevaland <deanh0716@gmail.com>
2021-09-27 05:56:49 +0900

    Implement to replace hash to alias with color

--- background.js
+++ background.js
@@ -1,6 +1,18 @@
-let color = "#3aa757";
+var hashs = {
+  "0xe206e3DCa498258f1B7EEc1c640B5AEE7BB88Fd0": {
+    name: "TEST-ADDR",
+    color: "red",
+  },
+  "0x46f26fe7447917c2abb8134719fd24dd90a2fc1248248fb1f6a56c85ef830a7d": {
+    name: "TXN-HASH",
+    color: "green",
+  },
+  "0xe206e3dca498258f1b7eec1c640b5aee7bb88fd0": {
+    name: "OVER-TEST",
+    color: "yellow",
+  },
+};
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log("Default background color set to %cgreen", `color: ${color}`);
+  chrome.storage.sync.set({ hashs });
 });
--- popup.js
+++ popup.js
@@ -1,17 +1,50 @@
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
+buttonReplace.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    function: replaceHashsToAlias,
   });
 });
 
-// The body of this function will be executed as a content script inside the
-// current page
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
+function replaceHashsToAlias() {
+  chrome.storage.sync.get("hashs", ({ hashs }) => {
+    var regex = /^0x[A-Fa-f0-9]{40,64}$/;
+
+    let span_elements = document.querySelectorAll("span");
+    for (let elem of span_elements) {
+      if (regex.test(elem.innerHTML) && hashs[elem.innerHTML]) {
+        let hash = hashs[elem.innerHTML];
+        elem.innerHTML =
+          '<span style="color:' + hash.color + '">' + hash.name + "</span>";
+      }
+    }
+
+    let a_elements = document.querySelectorAll("a");
+    for (let elem of a_elements) {
+      if (regex.test(elem.innerHTML) && hashs[elem.innerHTML]) {
+        let hash = hashs[elem.innerHTML];
+        elem.innerHTML =
+          '<span style="color:' + hash.color + '">' + hash.name + "</span>";
+      }
+    }
+    console.log(hashs);
   });
 }
+
+function constructHashs() {
+  chrome.storage.sync.get("hashs", ({ hashs }) => {
+    let ul = document.getElementsByClassName("app-content__alias-list");
+    for (let hash in hashs) {
+      let li_item = document.createElement("li");
+      li_item.innerHTML = `${hashs[hash].name}[${hash.substring(
+        0,
+        10
+      )}]: <span style="color:${hashs[hash].color};">${
+        hashs[hash].color
+      }</span>`;
+      ul[0].appendChild(li_item);
+    }
+  });
+}
+constructHashs();
